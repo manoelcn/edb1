4807322dd651ffb0a51c535c36a2dd04bb1e69da
manoel cândido <candidomanoel78@gmail.com>
2026-04-18 15:53:29 -0300

    selection sort

---
+++ anotacoes/ordenacao/SelectionSort/selectionSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+void selectionSort(int lista[], int tamanho)
+{
+    for (int i = 0; i < tamanho; i++) // i representa a fronteira entre a parte ordenada e a parte não ordenada
+    {
+        int indiceMenor = i; // assume que o primeiro elemento da parte não ordenada é o mínimo
+
+        for (int j = i + 1; i < tamanho; j++) // varre o restante do array para encontrar o verdadeiro mínimo
+        {
+            if (lista[j] < lista[indiceMenor])
+            {
+                indiceMenor = j; // atualiza o índice do menor valor encontrado
+            }
+
+            if (indiceMenor != i) // se o mínimo não está na posição i, troca os elementos
+            {
+                int temp = lista[i];
+                lista[i] = lista[indiceMenor];
+                lista[indiceMenor] = temp;
+            }
+        }
+    }
+}
---
+++ anotacoes/ordenacao/SelectionSort/selectionSort.md
@@ -0,0 +1,5 @@
+# Selection Sort
+
+Divide o array em ordenado e não ordenado. A cada passo, encontra o mínimo da parte não ordenada (menor elemento) e o troca com o primeiro elemento dessa parte.
+
+Complexidade O(n²) em todos os casos.
