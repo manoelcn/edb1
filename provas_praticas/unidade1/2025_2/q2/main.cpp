d55b2c672706b1de7c2d8802873e36cfe4f701a8
manoel cândido <candidomanoel78@gmail.com>
2026-04-29 09:13:01 -0300

    questão 2

---
+++ provas_praticas/unidade1/2025_2/q2/main.cpp
@@ -0,0 +1,140 @@
+#define __CHECK_INTEGRIDADE__ 1
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../doctest.h"
+#include "DadosEntrada.h"
+#include <vector>
+
+using namespace std;
+
+void trocar(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int medianaDe3(int arr[], int esquerda, int direita)
+{
+    int meio = esquerda + (direita - esquerda) / 2;
+
+    if (arr[esquerda] > arr[meio])
+    {
+        trocar(arr, esquerda, meio);
+    }
+
+    if (arr[esquerda] > arr[direita])
+    {
+        trocar(arr, esquerda, direita);
+    }
+
+    if (arr[meio] > arr[direita])
+    {
+        trocar(arr, meio, direita);
+    }
+
+    trocar(arr, meio, direita);
+
+    return arr[direita];
+}
+
+int particionar(int arr[], int esquerda, int direita)
+{
+    int pivo = arr[direita];
+    int indiceMenor = esquerda - 1;
+
+    for (int i = esquerda; i < direita; i++)
+    {
+        if (arr[i] >= pivo)
+        {
+            indiceMenor++;
+            trocar(arr, indiceMenor, i);
+        }
+    }
+
+    int posicaoFinalPivo = indiceMenor + 1;
+    trocar(arr, posicaoFinalPivo, direita);
+
+    return posicaoFinalPivo;
+}
+
+void quickSort(int arr[], int esquerda, int direita) // versão: EAMB-1.0
+{                                                    // Begin ​
+    if (esquerda >= direita)
+    {
+        return;
+    }
+
+    medianaDe3(arr, esquerda, direita);
+
+    int pivo = particionar(arr, esquerda, direita);
+
+    quickSort(arr, esquerda, pivo - 1);
+
+    quickSort(arr, pivo + 1, direita);
+
+} // End ​
+
+/**
+ * Não altere o código abaixo.
+ * Sua solução deve ser implementada na função mergeSort.
+ */
+// Função auxiliar para verificar ordem decrescente
+bool estaDecrescente(int array[], int tamanho)
+{
+    for (int i = 0; i < tamanho - 1; i++)
+    {
+        if (array[i] < array[i + 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Função auxiliar ao teste
+void CHECK_EQUALS(const vector<int> &entrada, const vector<int> &esperado)
+{
+    vector<int> copia = entrada;
+    int *arr = copia.data();
+    quickSort(arr, 0, copia.size() - 1);
+    CHECK(copia == esperado);
+}
+
+//  Testes
+TEST_CASE("Casos básicos")
+{
+    CHECK_EQUALS({}, {});
+    CHECK_EQUALS({1}, {1});
+    CHECK_EQUALS({1, 2}, {2, 1});
+}
+
+TEST_CASE("Casos médios")
+{
+    CHECK_EQUALS({3, 1, 4, 2}, {4, 3, 2, 1});
+    CHECK_EQUALS({5, 3, 8, 4, 2}, {8, 5, 4, 3, 2});
+    CHECK_EQUALS({10, 9, 8, 7, 6, 5}, {10, 9, 8, 7, 6, 5});
+}
+
+TEST_CASE("Casos com repetidos e negativos")
+{
+    CHECK_EQUALS({1, 3, 3, 2, 2, 1}, {3, 3, 2, 2, 1, 1});
+    CHECK_EQUALS({-3, -1, -2, 0}, {0, -1, -2, -3});
+    CHECK_EQUALS({1000, -100, 500, 0, -1, 999}, {1000, 999, 500, 0, -1, -100});
+}
+
+TEST_CASE("Caso array pequeno e aleatório")
+{
+    int t1 = sizeof(arrayPequeno) / sizeof(int);
+    quickSort(arrayPequeno, 0, t1 - 1);
+
+    CHECK(estaDecrescente(arrayPequeno, t1));
+}
+
+TEST_CASE("Caso array grande e aleatório")
+{
+    int t1 = sizeof(arrayGrande) / sizeof(int);
+    quickSort(arrayGrande, 0, t1 - 1);
+
+    CHECK(estaDecrescente(arrayGrande, t1));
+}
