5715c96a82a4568202569e417bae5d43dd673b9d
manoel cândido <candidomanoel78@gmail.com>
2026-06-05 17:02:21 -0300

    adiciona tad fila

---
+++ estruturas/fila-array-main/header/Fila.h
@@ -0,0 +1,80 @@
+//
+//  Fila.h
+//
+//  Criado por Eiji Adachi Medeiros Barbosa
+//
+
+#ifndef __Fila__
+#define __Fila__
+
+#include <string>
+#include <sstream>
+template <typename T>
+class Fila
+{
+private:
+    T* elementos;
+    int capacidade;
+    int quantidade;
+    int inicio;
+    int fim;
+    
+public:
+    Fila(int cap)
+    {
+        this->quantidade = 0;
+        this->inicio = 0;
+        this->fim = 0;
+        this->capacidade = cap;
+        this->elementos = new T[this->capacidade];
+    }
+    
+    ~Fila()
+    {
+        delete [] this->elementos;
+    }
+    
+    int tamanho()
+    {
+        return this->quantidade;
+    }
+
+    T frente()
+    {
+    }
+
+    void enfileirar(T elemento)
+    {
+    }
+
+    T desenfileirar()
+    {
+    }
+
+    bool cheia()
+    {
+        return this->quantidade == this->capacidade;
+    }
+
+    bool vazia()
+    {
+        return this->quantidade == 0;
+    }
+
+    std::string imprimir()
+    {
+        std::stringstream resultado;
+        resultado << "{ ";
+        int i = this->inicio;
+        int count = 0;
+        while (count < this->quantidade) {
+            resultado << this->elementos[i] << " ";
+            i = (i + 1) % this->capacidade;
+            ++count;
+        }
+        resultado << "}";
+        return resultado.str();
+    }
+};
+
+#endif
