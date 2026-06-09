db27f82d48f5ed4460fccdda3b2138189b3d1fd8
manoel cândido <candidomanoel78@gmail.com>
2026-06-09 10:24:33 -0300

    implementa questão 2

--- provas_praticas/unidade2/2023_2/q2/src/ListaEncadeada.cpp
+++ provas_praticas/unidade2/2023_2/q2/src/ListaEncadeada.cpp
@@ -37,7 +37,40 @@ void ListaEncadeada::inserirNoInicio(std::string elemento)
 
 bool ListaEncadeada::remover(std::string val)
 {
-    throw "\n!!! MÉTODO 'remover' AINDA NÃO FOI IMPLEMENTADO !!! \n";
+	auto atual = this->inicio;
+	No<std::string> *anterior = nullptr;
+	while (atual != nullptr)
+	{
+		if (atual->getValor() == val)
+		{
+			if (anterior == nullptr)
+			{
+				auto remover = atual;
+				this->inicio = atual->getProximo();
+				delete remover;
+				this->quantidade--;
+				return true;
+			}
+			if (atual->getProximo() == nullptr)
+			{
+				auto remover = atual;
+                delete remover;
+                anterior->setProximo(nullptr);
+                atual = anterior;
+                this->quantidade--;
+                return true;
+			}
+			auto remover = atual;
+            anterior->setProximo(atual->getProximo());
+            atual = atual->getProximo();
+            delete remover;
+            this->quantidade--;
+            return true;
+		}
+		anterior = atual;
+		atual = atual->getProximo();
+	}
+	return false;
 }
 
 bool ListaEncadeada::vazia()
